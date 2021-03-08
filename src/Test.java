371ab0ff5a014a9d157aa45320b6dd4d0bb1fc57
RandomKiddo <nghugare2@outlook.com>
2021-03-07 20:55:26 -0500

    Created Test Files

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+/*
+Jex, Copyright 2021 RandomKiddo
+
+This work is licensed under the GNU General Public License (or GNU GPL), version 3.0
+This program comes with ABSOLUTELY NO WARRANTY
+This is a free software, and you are welcome to modify and redistribute it under certain conditions.
+
+The current maintainer of this work is RandomKiddo
+The "work" is defined as any part of Jex that is a source code file (.java, .jar, etc.)
+
+For more information on the license, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
